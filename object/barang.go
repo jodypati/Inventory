7623374330400b47b71f604fd22fbe5dc3e41165
Sausa Jodypati <jodypati@gmail.com>
2018-02-20 08:07:04 +0700

    fix datetime formatting

--- object/barang.go
+++ object/barang.go
@@ -34,6 +34,17 @@ type Detail struct {
 	Total float64 `gorm:"not null" form:"total" json:"total"`
 }
 
+type SalesHeader struct {
+	Date string `gorm:"not null" form:"date" json:"date"`
+	DateFrom string `gorm:"not null" form:"datefrom" json:"datefrom"`
+	DateTo string `gorm:"not null" form:"dateto" json:"dateto"`
+	Turnover float64 `gorm:"not null" form:"turnover" json:"turnover"`
+	GrossProfit float64 `gorm:"not null" form:"grossprofit" json:"grossprofit"`
+	TotalSales int `gorm:"not null" form:"totalsales" json:"totalsales"`
+	TotalGoods int `gorm:"not null" form:"totalgoods" json:"totalgoods"`
+	SalesDetail *[]SalesDetail `gorm:"not null" form:"detail" json:"detail"`
+}
+
 type SalesDetail struct {
     BookingId 		string `gorm:"not null" form:"bookingid" json:"bookingid"`
     Time			time.Time `gorm:"not null" form:"time" json:"time"`
@@ -195,8 +206,51 @@ func OptionsBarang(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	c.Next()
 }
+func ImportBarang(c *gin.Context){
+	filename := "barang.csv"
 
-func ImportValueReport(c *gin.Context){
+    // Open CSV file
+    f, err := os.Open(filename)
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+
+    // Read File into a Variable
+    r := csv.NewReader(f)
+    r.Comma = (';')
+    lines, err := r.ReadAll()
+    if err != nil {
+        panic(err)
+		// Display error
+		c.JSON(422, gin.H{"error": err})
+	}else{
+		db := InitDbBarang()
+		defer db.Close()
+
+		var barangs []Barangs
+		// Loop through lines & turn into object
+    	for _, line := range lines {
+    		
+    		jumlah, err := strconv.Atoi(line[2])
+    		if err == nil {
+	        	barang := Barangs{
+			        SKU: line[0],
+			        ItemName: line[1],
+			        Quantity: jumlah,
+		        } 
+		        db.Create(&barang)
+		        barangs = append(barangs,barang)
+	    	}else{
+	    		c.JSON(422, gin.H{"error": err})		
+	    	}
+	    }
+		
+		// Display error
+		c.JSON(200, gin.H{"success": barangs})
+	}
+}
+func ExportValueReport(c *gin.Context){
 	db := InitDbBarang()
 	// Close connection database
 	defer db.Close()
@@ -270,32 +324,54 @@ func SalesReport(c *gin.Context) {
 	db := InitDbBarang()
 	// Close connection database
 	defer db.Close()
+	const dateFormat = "2006-01-02"
+	dateFrom, _ := time.Parse(dateFormat, c.Params.ByName("datefrom"))
+	dateTo, _ := time.Parse(dateFormat, c.Params.ByName("dateto"))
 	var salesDetail []SalesDetail
 	// var goodsAmount float64
 	// var valueTotal float64
 	
-	db.Table("barangkeluars").Select("barangkeluars.receipt_number,barangkeluars.time,barangkeluars.sku,barangkeluars.item_name,barangkeluars.stock_out,barangkeluars.selling_price,barangkeluars.selling_price*barangkeluars.stock_out as total_sale, SUM(barangmasuks.total)/SUM(amount_recieved) as sale_price, (barangkeluars.selling_price*barangkeluars.stock_out) - (barangkeluars.stock_out * (SUM(barangmasuks.total)/SUM(amount_recieved))) as profit").Joins("inner join barangmasuks on barangkeluars.SKU=barangmasuks.SKU").Group("barangkeluars.receipt_number,barangkeluars.time,barangkeluars.sku,barangkeluars.item_name,barangkeluars.stock_out,barangkeluars.selling_price").Scan(&salesDetail)
-	c.JSON(200, gin.H{"success": salesDetail})
-	// itemsAmount := 0;
-	// for _, detail := range details {
- //        goodsAmount += detail.AveragePrice
- //        valueTotal += detail.Total
- //        itemsAmount++
-	// }
+	db.Table("barangkeluars").Select("barangkeluars.receipt_number as booking_id,barangkeluars.time,barangkeluars.sku,barangkeluars.item_name,barangkeluars.stock_out as quantity,barangkeluars.selling_price as sale_price,barangkeluars.selling_price*barangkeluars.stock_out as total_sale, SUM(barangmasuks.total)/SUM(amount_recieved) as purchase_price, (barangkeluars.selling_price*barangkeluars.stock_out) - (barangkeluars.stock_out * (SUM(barangmasuks.total)/SUM(amount_recieved))) as profit").Joins("inner join barangmasuks on barangkeluars.SKU=barangmasuks.SKU").Where("barangkeluars.time >= ? and barangkeluars.time <= ?",dateFrom,dateTo).Group("barangkeluars.receipt_number,barangkeluars.time,barangkeluars.sku,barangkeluars.item_name,barangkeluars.stock_out,barangkeluars.selling_price").Scan(&salesDetail)
+	
+	totalGoods := 0
+	totalSales := 0
+	turnover := 0.0
+	grossProfit := 0.0
+	for _, detail := range salesDetail {
+        totalGoods += detail.Quantity
+        if detail.BookingId != ""{
+        	totalSales += detail.Quantity
+        }
+        turnover += detail.TotalSale
+        grossProfit += detail.Profit
+	}
 
 
-	// t := time.Now()
-	// result := HeaderReport {
-	// 	Date: t.Format("20060102"),
-	// 	ItemsAmount: itemsAmount,
-	// 	GoodsAmount: goodsAmount,
-	// 	ValueTotal: valueTotal,
-	// 	Detail: &details,
-	// }
-	// if itemsAmount > 0 {
-	// // Display modified data in JSON message "success"
-	// 	c.JSON(200, gin.H{"success": result})
-	// }else{
-	// 	c.JSON(404, gin.H{"error": "Report not found"})
-	// }
+	t := time.Now()
+	result := SalesHeader {
+		Date: t.Format("02 Jan 2006"),
+		DateFrom: dateFrom.Format("02 Jan 2006"),
+		DateTo: dateTo.Format("02 Jan 2006"),
+		Turnover: turnover,
+		GrossProfit: grossProfit,
+		TotalSales: totalSales,
+		TotalGoods: totalGoods,
+		SalesDetail: &salesDetail,
+	}
+	if totalGoods > 0 {
+	// Display modified data in JSON message "success"
+		c.JSON(200, gin.H{"success": result})
+	}else{
+		c.JSON(404, gin.H{"error": "Report not found"})
+	}
+}
+
+func TruncateAll(c *gin.Context) {
+	//Connection to the database
+	db := InitDbBarang()
+	// Close connection database
+	defer db.Close()
+	db.Exec("DROP TABLE Barangs;")
+	db.Exec("DROP TABLE Barangmasuks;")
+	db.Exec("DROP TABLE Barangkeluars;")
 }
